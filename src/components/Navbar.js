63fc5a555a3b91b00ce85747718f6d2998855f7e
Julian <juliiansandstrom@gmail.com>
2024-10-01 14:57:19 +0200

    fixed shadows in current-info

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,5 +1,3 @@
-import { Link } from 'react-router-dom';
-
 function Navbar() {
   return (
     <header className="d-flex justify-content-between align-items-center p-3 px-md-4 bg-light border-bottom box-shadow">
